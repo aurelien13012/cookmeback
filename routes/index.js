1121d2986b2ae8c6bb0c3239ba48db67cd574834
ayoubouzzine <ayoubbouzzine@gmail.com>
2021-05-20 17:19:49 +0200

    Login_ mise en place des routes signin signup sur le fichier route users

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,6 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-const UserModel = require('../Models/users')
-
-const uid2 = require("uid2");
-const bcrypt = require("bcrypt");
-
 const recipeModel = require('../Models/recipes');
 const ingredientModel = require('../Models/ingredients');
 
@@ -15,90 +10,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-// route signup = pour s'inscrire
-router.post('/signup',  async (req, res, next)=>{
-
-  let error = []
-  let result = false
-  let saveUser = null
-  let token = null
-
-  const data = await UserModel.findOne({
-    email: req.body.emailFromFront
-  })
-
-  if(data != null){
-    error.push('utilisateur déjà présent')
-  }
-
-  if(req.body.firstNameFromFront == ''
-  || req.body.surNameFromFront == ''
-  || req.body.emailFromFront == ''
-  || req.body.passwordFromFront == ''
-  ){
-    error.push('champs vides')
-  }
-
-
-  if(error.length == 0){
-    const hash = bcrypt.hashSync(req.body.passwordFromFront, 10);
-
-    let newUser = new UserModel({
-      firstName: req.body.firstNameFromFront,
-      surName: req.body.surNameFromFront,
-      email: req.body.emailFromFront,
-      password: hash,
-      token : uid2(32),
-    })
-  
-    saveUser = await newUser.save()
-  
-    
-    if(saveUser){
-      result = true
-      token = saveUser.token
-    }
-  }
-
-  res.json({result, saveUser, error, token})
-})
-
-//route signin  = pour se loguer
-router.post('/signin', async (req, res, next)=>{
-
-  let result = false
-  let user = null
-  let error = []
-  let token = null
-  
-  if(req.body.emailFromFront == ''
-  || req.body.passwordFromFront == ''
-  ){
-    error.push('champs vides')
-  }
-
-  if(error.length == 0){
-    const user = await UserModel.findOne({
-      email: req.body.emailFromFront,
-    })
-  
-    
-    if(user){
-      if(bcrypt.compareSync(req.body.passwordFromFront, user.password)){
-        result = true
-        token = user.token
-      } else {
-        result = false
-        error.push('mot de passe incorrect')
-      }
-      
-    } else {
-      error.push('email incorrect')
-    }
-  }
-  
-  res.json({result, user, error, token})
-})
 
 //route recipeFromApi = renvoi des recette de l'api
 router.get('/recipesFromApi',  (req, res, next)=>{
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,100 @@
+const mongoose = require('mongoose');
 var express = require('express');
 var router = express.Router();
+
+const UserModel = require('../Models/users')
+
+const uid2 = require("uid2");
+const bcrypt = require("bcrypt");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+// route signup = pour s'inscrire
+router.post('/signup',  async (req, res, next)=>{
+
+  let error = []
+  let result = false
+  let saveUser = null
+  let token = null
+
+  const data = await UserModel.findOne({
+    email: req.body.emailFromFront
+  })
+
+  if(data != null){
+    error.push('utilisateur déjà présent')
+  }
+
+  if(req.body.firstNameFromFront == ''
+  || req.body.surNameFromFront == ''
+  || req.body.emailFromFront == ''
+  || req.body.passwordFromFront == ''
+  ){
+    error.push('champs vides')
+  }
+
+
+  if(error.length == 0){
+    const hash = bcrypt.hashSync(req.body.passwordFromFront, 10);
+
+    let newUser = new UserModel({
+      firstName: req.body.firstNameFromFront,
+      surName: req.body.surNameFromFront,
+      email: req.body.emailFromFront,
+      password: hash,
+      token : uid2(32),
+    })
+  
+    saveUser = await newUser.save()
+  
+    
+    if(saveUser){
+      result = true
+      token = saveUser.token
+    }
+  }
+
+  res.json({result, saveUser, error, token})
+})
+
+//route signin  = pour se loguer
+router.post('/signin', async (req, res, next)=>{
+
+  let result = false
+  let user = null
+  let error = []
+  let token = null
+  
+  if(req.body.emailFromFront == ''
+  || req.body.passwordFromFront == ''
+  ){
+    error.push('champs vides')
+  }
+
+  if(error.length == 0){
+    const user = await UserModel.findOne({
+      email: req.body.emailFromFront,
+    })
+  
+    
+    if(user){
+      if(bcrypt.compareSync(req.body.passwordFromFront, user.password)){
+        result = true
+        token = user.token
+      } else {
+        result = false
+        error.push('mot de passe incorrect')
+      }
+      
+    } else {
+      error.push('email incorrect')
+    }
+  }
+  
+  res.json({result, user, error, token})
+})
 
 module.exports = router;
