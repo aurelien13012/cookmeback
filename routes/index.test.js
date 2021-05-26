c567c36be790c18b3d223bbc954ef7a3b7247362
Aurélien Chevillotte <chevillotte.aurelien@gmail.com>
2021-05-26 18:00:50 +0200

    indextest_mise en place de deux routes pour test

--- routes/index.test.js
+++ routes/index.test.js
@@ -1,24 +1,20 @@
 var app = require("../app")
 var request = require("supertest")
 
+//test route pour sign up 
 test("name", async (done) => {
-    await request(app).post("/signup")
-    .send({ "firstname": "John", "lastName":"doe", "email" : 'john.doe@gmail.com',
-    "password" : 'azert' })
-    .expect(200)
-    .expect({ firstname : "John", lastName:"doe", email : 'john.doe@gmail.com',
-    password : 'azert' })
-    
+    await request(app).post("/signin")
+        .send({ "firstname": "John" })
+        .expect(200)
+        .expect({ prenom: "John" })
     done()
-    })
+})
 
-
-    //test route myrecipe mettre en place une entrée une sortie 
-    test("name", async (done) => {
-        await request(app).post("/myrecipe")
-        .send({ token})
+//test route myrecipe mettre en place une entrée une sortie 
+test("name", async (done) => {
+    await request(app).post("/infoUser")
+        .send({ token })
         .expect(200)
         .expect({ recipe })
-        
-        done()
-        })
+    done()
+})
